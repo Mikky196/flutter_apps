3f996d973639176569e4eb7cc15284478ae7745f
Micheal Adeniran <oluwapamilerinmicheal@gmail.com>
2023-03-28 11:12:22 +0100

    created and wrote the code for the project from scratch, added images and app icons

--- i_am_poor/lib/main.dart
+++ i_am_poor/lib/main.dart
@@ -3,17 +3,32 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.black26,
-          title: const Center(
-            child: Text('I Am Broke'),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey[900],
+            title: Center(
+              child: Text('I Am Poor'),
+            ),
           ),
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/stone.jpg'),
+          body: Center(
+            child: Image.asset('images/stone.png'),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            shape: CircularNotchedRectangle(),
+            child: Container(
+              color: Colors.blueGrey[900],
+              child: Center(
+                child: Text(
+                  'SAPA fe Pami',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              height: 50,
+            ),
           ),
         ),
       ),
