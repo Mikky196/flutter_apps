c5dfb7f4d0d8c23b509bd430db79611e1cc32106
Micheal Adeniran <oluwapamilerinmicheal@gmail.com>
2023-04-12 13:38:12 +0100

    worked on the xylophone app, added and used the audioplayers package, added functionalities to avoid code repetitions

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,7 +1,56 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(),
+    XylophoneApp(),
   );
+}
+
+class XylophoneApp extends StatefulWidget {
+  const XylophoneApp({Key key}) : super(key: key);
+
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  void playSound(int note) {
+    final player = AudioCache();
+    player.play('note$note.wav');
+  }
+
+  Expanded buildKey({Color color, int note}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(note);
+        },
+        child: null,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, note: 1),
+              buildKey(color: Colors.green, note: 2),
+              buildKey(color: Colors.blue, note: 3),
+              buildKey(color: Colors.yellow, note: 4),
+              buildKey(color: Colors.orange, note: 5),
+              buildKey(color: Colors.purple, note: 6),
+              buildKey(color: Colors.teal, note: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
