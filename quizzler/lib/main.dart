32ff4c529fc25034a9aa4a093c2b6bffe08501e2
Micheal Adeniran <oluwapamilerinmicheal@gmail.com>
2023-04-21 02:25:57 +0100

    made changes to quizzler project. I created the layout of the app.

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -16,6 +16,53 @@ class Quizzler extends StatefulWidget {
 class _QuizzlerState extends State<Quizzler> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black45,
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      "This is where the question text will go",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                    color: Colors.green,
+                    onPressed: () {},
+                    child: Text("True"),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                    color: Colors.red,
+                    onPressed: () {},
+                    child: Text("False"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
